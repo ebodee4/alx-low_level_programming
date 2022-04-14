8915c3f96c490e77448999183f0967fc061bf0c0
ebodee4 <101496129+ebodee4@users.noreply.github.com>
2022-04-14 18:45:09 +0100

    Update 0-putchar.c

--- 0x04-more_functions_nested_loops/0-putchar.c
+++ 0x04-more_functions_nested_loops/0-putchar.c
@@ -4,29 +4,16 @@
  *
  * Return: nothing, return void
  */
-
 int main(void)
-  
 {
-  
-  _putchar('_');
-  
-  _putchar('p');
-  
-  _putchar('u');
-  
-  _putchar('t');
-  
-  _putchar('c');
-  
-  _putchar('h');
-  
-  _putchar('a');
-  
-  _putchar('r');
-  
-  _putchar('\n');
-  
-  return (0);
-  
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
